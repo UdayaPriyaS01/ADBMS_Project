fd1efe8d7e21029f35807d152bce346ef813a1c0
UdayaPriyaS01 <43386341+UdayaPriyaS01@users.noreply.github.com>
2022-11-05 17:18:01 -0400

    1105

---
+++ create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE adult_tbl ( age numeric,workclass varchar(50),fnlwgt numeric,
+						education varchar(50),education_num numeric,marital_status varchar(50),
+						occupation varchar(50),relationship varchar(50),race varchar(50),sex varchar(50),
+						capital_gain numeric,capital_loss numeric,hours_per_week numeric,native_country
+						varchar(50),salary_class varchar(50)  )
+						
